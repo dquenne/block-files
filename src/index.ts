811290abb6a2e02cf40bf07290247b43bf58184b
Dylan Quenneville <dquenne@gmail.com>
2020-04-26 14:20:33 -0700

    Add src/index.ts for simpler imports

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { ITable } from "./lib/Table/ITable.ts";
+export { FileTable, Serializer, Deserializer } from "./lib/Table/FileTable.ts";
+
+export { IRowPointer } from "./lib/RowPointer/IRowPointer.ts";
+export { RowPointer } from "./lib/RowPointer/RowPointer.ts";
+
+export { IBufferHandler } from "./lib/BufferHandler/IBufferHandler.ts";
+export { FileBufferHandler } from "./lib/BufferHandler/FileBufferHandler.ts";
